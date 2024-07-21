3af7fc33703bf1eea38c491ab0cbbd1f511a150c
ZiyouZoeZhang <58147818+ZiyouZoeZhang@users.noreply.github.com>
2024-07-21 21:16:38 +0200

    2024/07/21

--- 2024/USACO_prep/class/graphs/07_21/bfs.cpp
+++ 2024/USACO_prep/class/graphs/07_21/bfs.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 #define INF 0x3f3f3f3f
+#define ll long long
 using namespace std;
+
 vector<int> adj[200010];
 bool visited[200010];
 int dist[200010];
@@ -13,22 +15,19 @@ void bfs(int x){
     q.push(x);
     while (!q.empty()){
         int a=q.front(); q.pop();
-        if (visited[a]) continue;
-        visited[a]=true;
         for (auto b: adj[a]){
+            if (visited[b]) continue;
+            visited[b]=true;
+            dist[b]=dist[a]+1;
+            parent[b]=a;
             q.push(b);
-            if (dist[a]+1<dist[b]){
-                dist[b]=dist[a]+1;
-                parent[b]=a;
-            }
-            
         }
     }
 }
 
 int main(){
-    memset(dist, INF, 200010);
-    memset(parent, INF, 200010);
+    memset(dist, INF, sizeof(dist));
+    //memset(parent, INF, 200010);
 
     //updated
     cin>>n>>m;
@@ -38,7 +37,13 @@ int main(){
         adj[a].push_back(b);
         adj[b].push_back(a);
     }
+
     bfs(1);
+
+    if (dist[n]==INF){
+        cout<<"IMPOSSIBLE"<<endl; 
+        return 0;
+    }
     
     cout<<dist[n]<<endl;
 
