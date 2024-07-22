7a6731d53138639c030587c1f6590fccacb8bd65
ZiyouZoeZhang <58147818+ZiyouZoeZhang@users.noreply.github.com>
2024-07-23 00:36:23 +0200

    2024/07/23

--- 2024/class/graphs/07_21/test.cpp
+++ 2024/class/graphs/07_21/test.cpp
@@ -1,16 +1,17 @@
 #include <bits/stdc++.h>
+#define ll long long
 #define INF 0x3f3f3f3f
 
 using namespace std;
 
-vector<pair<int, int>> adj[10010];
-int dist[10010];
-bool vis[100010];
-int n, m;
+vector<pair<ll, ll>> adj[200001];
+ll dist[200001];
+bool vis[200001];
+ll n, m;
 
 
-void dijkastra(int x){
-    priority_queue<pair<int, int>> pq;
+void dijkastra(ll x){
+    priority_queue<pair<ll, ll>> pq;
 
     memset(dist, INF, sizeof(dist));
     memset(vis, false, sizeof(vis));
@@ -18,11 +19,11 @@ void dijkastra(int x){
     pq.push(make_pair(0, x));
     dist[x]=0;
     while (!pq.empty()){
-        int a=pq.top().second; pq.pop();
+        ll a=pq.top().second; pq.pop();
         if (vis[a]==true) continue;
         vis[a]=true;
         for (auto i: adj[a]){
-            int b=i.first, w=i.second;
+            ll b=i.first, w=i.second;
             dist[b] = min(dist[b], dist[a]+w);
             pq.push(make_pair(-dist[b], b));
         }
@@ -55,16 +56,18 @@ void bfs(int x){
 
 int main (){
     cin>>n>>m;
-    for (int i=1; i<=m; i++){
-        int a, b, c;
+    for (ll i=1; i<=m; i++){
+        ll a, b, c;
         cin>>a>>b>>c;
         adj[a].push_back({b, c});
     }
 
     dijkastra(1);
-    for (int i=1; i<= n; i++){
+    for (ll i=1; i<= n; i++){
         cout<<dist[i]<<" ";
     }
+
+    return 0;
 
 }
 
