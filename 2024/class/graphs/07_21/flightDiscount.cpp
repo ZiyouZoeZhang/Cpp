0c5a096981cc040e1d4eff3dc80c0d6328bb3219
ZiyouZoeZhang <58147818+ZiyouZoeZhang@users.noreply.github.com>
2024-07-27 11:04:47 +0200

    2024/07/27

--- 2024/class/graphs/07_21/flightDiscount.cpp
+++ 2024/class/graphs/07_21/flightDiscount.cpp
@@ -14,8 +14,8 @@ vector<ll> distB;
 //visA, vis B
 
 vector<ll> dij(ll x){
-    vector<ll> dist (M+2, INF);
-    vector<bool> vis (M+2, false);
+    vector<ll> dist (N+2, INF);
+    vector<bool> vis (N+2, false);
     priority_queue<pair<ll, ll>> pq;
 
     pq.push(make_pair(0, x));
@@ -36,40 +36,17 @@ vector<ll> dij(ll x){
 }
 
 
-vector<ll> dij_rev(ll x){
-    vector<ll> dist (M+2, INF);
-    vector<bool> vis (M+2, false);
-    priority_queue<pair<ll, ll>> pq;
-
-    pq.push(make_pair(0, x));
-    dist[x]=0;
-
-    while (!pq.empty()){
-        ll a=pq.top().second; pq.pop();
-        if (vis[a]) continue;
-        vis[a]=true;
-
-        for (auto i: adj_rev[a]){
-            ll b=i.first, w=i.second;
-            dist[b]=min(dist[b], dist[a]+w);
-            pq.push(make_pair(-dist[b], b));
-        }
-    }
-
-    return dist;
-}
-
-
 int main(){
+
     cin>>N>>M;
     for (ll i=0; i<M; i++){
         ll a, b, c;
         cin>>a>>b>>c;
         adj[a].push_back(make_pair(b, c));
-        adj_rev[b].push_back(make_pair(a, c));
+        adj[b].push_back(make_pair(a, c));
     }
 
-    vector<ll> distA = dij(1), distB = dij_rev(N);
+    vector<ll> distA = dij(1), distB = dij(N);
 
     ll minimum=INF;
     for (ll a=1; a<=M; a++) {
