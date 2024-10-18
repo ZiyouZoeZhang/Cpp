2e9d0cdc365ff1f199c0a2b675fd9e50538abb7f
ZiyouZoeZhang <58147818+ZiyouZoeZhang@users.noreply.github.com>
2024-10-19 01:26:41 +0200

    2024/10 all

--- 2024/class/trees/09_28/irrigation_correct.cpp
+++ 2024/class/trees/09_28/irrigation_correct.cpp
@@ -1,3 +1,5 @@
+//to be studied
+
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
@@ -5,14 +7,18 @@ const int N=500001;
 int n,m,q,c[N];
 ll a[N],k;
 int main(){
-    scanf("%d%d%d",&n,&m,&q);
-    for(int x,i=1;i<=n;++i)
-        scanf("%d",&x),a[i]=(ll)(c[x]++)*m+x;
-    sort(a+1,a+n+1);
-    for(int i=1;i<=n;++i)a[i]-=i;
+    cin>>n>>m>>q;
+    for(int x,i=1;i<=n;++i){
+        cin>>x; //olympiad hold in city x; m cities in Berland
+        a[i] = (c[x]++)*m+x; //year[i]= (count+1)*cities+citynum
+    }
+    sort(a+1,a+n+1); //sort years
+    for(int i=1;i<=n;++i){
+        a[i]-=i; 
+    }
     while(q--){
-        scanf("%lld",&k);
-        k+=lower_bound(a+1,a+n+1,k-n)-a-1-n;
-        printf("%lld\n",(k-1)%m+1);
+        cin>>k;
+        k+=lower_bound(a+1,a+n+1,k-n)-(a+1)-n;
+        cout<<(k-1) % m + 1;
     }
 }
